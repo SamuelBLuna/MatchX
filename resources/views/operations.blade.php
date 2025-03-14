de1804a58e3f016f6994fac067115eb8994d0986
Samuel Luna <samuhluna@gmail.com>
2025-03-13 23:28:06 -0300

    correção, footer duplicado no blade operations e finalização do projeto

--- resources/views/operations.blade.php
+++ resources/views/operations.blade.php
@@ -54,11 +54,6 @@
 
     <!-- footer -->
     <footer class="text-center mt-5">
-        <p class="text-secondary">MathX &copy; <span class="text-info">[ANO]</span></p>
-    </footer>
-
-    <!-- footer -->
-    <footer class="text-center mt-5">
         <p class="text-secondary">MathX &copy; <span class="text-info">{{ date('Y') }}</span></p>
     </footer>
 
